2f0f86579585e4c66500b5839683c4c0034de0ea
Christopher Dorst <chris@dorst.email>
2018-01-01 03:03:55 -0600

    Add helpers

---
+++ Metaproject.Cdn.Containers.VersionControl.Helpers/ContentConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Metaproject.Cdn.Containers.VersionControl.Helpers
+{
+    internal static class ContentConstants
+    {
+        public const string GitAttributesIgnore = "git_attributes_ignore.zip";
+        public const string GnuGpl3License = "gnu_license.zip";
+    }
+}
---
+++ Metaproject.Cdn.Containers.VersionControl.Helpers/VersionControlContentHelper.cs
@@ -0,0 +1,17 @@
+﻿using DevOps.ContentDelivery.Structures.AzureBlob;
+using static Metaproject.Cdn.Common.Helpers.CDorstCdnHelper;
+using static Metaproject.Cdn.Containers.VersionControl.Helpers.ContentConstants;
+
+namespace Metaproject.Cdn.Containers.VersionControl.Helpers
+{
+    public static class VersionControlContentHelper
+    {
+        private const string Container = nameof(VersionControl);
+
+        public static void SaveGitAttributeIgnoreFiles(string destinationPath)
+            => ExtractZip(new Blob(Container, GitAttributesIgnore), destinationPath);
+
+        public static void SaveGnuGpl3License(string destinationPath)
+            => ExtractZip(new Blob(Container, GnuGpl3License), destinationPath);
+    }
+}
